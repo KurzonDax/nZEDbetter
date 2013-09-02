fdfb549a9edbfb1143cf50339daa86d9278b51d9
Kurzon Dax <randy@thewilliscrew.org>
2013-09-01 21:26:54 -0500

    Initial commit.  Script allows you to reprocess a whole category back through determineCategory

---
+++ misc/testing/DB_scripts/recategorize.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Project: nZEDbetter
+ * User: Randy
+ * Date: 9/1/13
+ * Time: 6:34 PM
+ * File: recategorize.php
+ * This script allows you to specify a category ID in which all releases will be reprocessed through the
+ * determineCategory function.  This is handy if you've been tweaking the regex in category.php and want
+ * to reprocess the Other->Misc or Other->Hashed categories, or if you've ended up with a bunch of releases
+ * in a wrong category for some reason.
+ */
+
+require(dirname(__FILE__)."/../../../www/config.php");
+require_once(WWW_DIR."lib/framework/db.php");
+require_once(WWW_DIR."lib/category.php");
+require_once(WWW_DIR."lib/consoletools.php");
+
+$db = new DB();
+$category = new Category();
+$consoletools = new ConsoleTools();
+$gotIt = false;
+do {
+
+    $catToProcess = $consoletools->getUserInput("\nPlease enter the category ID that you want to reprocess or type quit to exit: ");
+    if(is_numeric($catToProcess) && $category->getById($catToProcess) != false)
+        $gotIt = true;
+    elseif($catToProcess=='quit')
+        exit ("\nThanks for playing.  We'll see you next time.\n");
+    else
+        echo "\n\nYou specified an invalid category ID.  Please try again.\n";
+
+} while ($gotIt==false);
+
+if($relsToProcess = $db->queryDirect("SELECT ID, name, searchname, groupID, categoryID FROM releases WHERE categoryID=".$catToProcess))
+{
+    echo "\n";
+    $relsCount = $db->getNumRows($relsToProcess);
+    $relsProcessed = 0;
+    $relsChanged = 0;
+    while($relRow=$db->fetchAssoc($relsToProcess))
+    {
+        $relsProcessed ++;
+        $newCategory = $category->determineCategory($relRow['name'],$relRow['groupID']);
+        $consoletools->overWrite("Working on release ".$consoletools->percentString($relsProcessed, $relsCount)." Updates: ".$relsChanged);
+        if($newCategory != $relRow['categoryID'])
+        {
+            $db->query("UPDATE releases SET categroyID=".$newCategory." WHERE ID=".$relRow['ID']);
+            file_put_contents(WWW_DIR."lib/logging/recategorize.log",$relRow['ID'].", '".$relRow['searchname']."', ".$newCategory."\n", FILE_APPEND);
+            $relsChanged ++;
+        }
+    }
+}
+exit ("Thanks for playing. Total releases changed: ".number_format($relsChanged)."\nThe log file can be found at: ".WWW_DIR."lib/logging/recategorize.log\n");
