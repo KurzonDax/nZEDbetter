4fed6e7f9cb63a8b4dcd5d70cc8fac17da774a30
Kurzon Dax <randy@thewilliscrew.org>
2013-09-26 13:33:44 -0500

    Added Minify functionality. Adding some files that were not previously committed.

---
+++ www/min/groupsConfig.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+    // 'js' => array('//js/file1.js', '//js/file2.js'),
+    // 'css' => array('//css/file1.css', '//css/file2.css'),
+    'themeJS' => array('//themes/Cyborg/scripts/bootstrap.min.js', '//themes/Cyborg/scripts/holder.js', '//themes/Cyborg/scripts/jquery.pnotify.min.js', '//themes/Cyborg/scripts/jquery.qtip.js', '//themes/Cyborg/scripts/jquery.autosize-min.js', '//themes/Cyborg/scripts/jquery.colorbox-min.js', '//themes/Cyborg/scripts/sorttable.js', '//themes/Cyborg/scripts/utils.js', '//themes/Cyborg/scripts/jquery.autocomplete.js'),
+);
