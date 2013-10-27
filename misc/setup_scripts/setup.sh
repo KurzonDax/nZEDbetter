abadd151d4c7e829c6c922f84a3c2347420af16b
Kurzon Dax <randy@thewilliscrew.org>
2013-10-27 17:51:34 -0500

    Fixed a couple of bugs in the setup shell script

--- misc/setup_scripts/setup.sh
+++ misc/setup_scripts/setup.sh
@@ -1,11 +1,13 @@
-sudo apt-get -y install openssh-server proftpd htop unrar software-properties-common tmux
+#!/bin/bash
 sudo add-apt-repository ppa:ondrej/php5
 sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 echo "deb http://repo.percona.com/apt raring main" | sudo tee -a /etc/apt/sources.list
 echo "deb-src http://repo.percona.com/apt raring main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
+sudo apt-get -y install openssh-server proftpd htop unrar software-properties-common tmux
+sudo apt-get -y install nmon vnstat tcptrack bwm-ng mytop
 sudo apt-get -y install percona-server-server-5.6 percona-toolkit
-sudo apt-get install apache2
+sudo apt-get -y install apache2
 sudo a2enmod rewrite
 sudo service apache2 restart
 sudo apt-get -y install php5 php5-dev php-pear php5-gd php5-mysql php5-curl php5-xdebug php5-mcrypt
@@ -53,7 +55,8 @@ git clone https://github.com/jonnyboy/powerline-fonts.git
 sudo gnome-font-viewer ~/Downloads/powerline-fonts/Consolas/Consolas\ for\ Powerline.ttf
 sudo cp /var/www/nZEDbetter/misc/setup_scripts/nzedbetter.conf /etc/apache2/sites-available/nZEDbetter.conf
 sudo a2ensite nZEDbetter
-sudo service apache2 restart
+sudo a2dissite 000-default.conf
+sudo service apache2 reload
 cd ~/Downloads
 wget http://mediaarea.net/download/binary/libzen0/0.4.29/libzen0_0.4.29-1_i386.xUbuntu_13.04.deb
 wget http://mediaarea.net/download/binary/libmediainfo0/0.7.64/libmediainfo0_0.7.64-1_i386.xUbuntu_13.04.deb
@@ -61,6 +64,7 @@ wget http://mediaarea.net/download/binary/mediainfo/0.7.64/mediainfo_0.7.64-1_i3
 sudo dpkg -i libzen0_0.4.29-1_i386.xUbuntu_13.04.deb
 sudo dpkg -i libmediainfo0_0.7.64-1_i386.xUbuntu_13.04.deb
 sudo dpkg -i mediainfo_0.7.64-1_i386.Debian_7.0.deb
+sudo apt-get -f -y install
 mkdir ~/ffmpeg_sources
 cd ~/ffmpeg_sources
 wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
@@ -72,6 +76,7 @@ make install
 make distclean
 cd ~
 . ~/.profile
+export PATH=$HOME/bin:$PATH
 cd ~/ffmpeg_sources
 git clone --depth 1 git://git.videolan.org/x264.git
 cd x264
@@ -108,6 +113,9 @@ git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
 export PKG_CONFIG_PATH
+cd ~
+. ~/.profile
+cd ~/ffmpeg_sources/ffmpeg
 ./configure --prefix="$HOME/ffmpeg_build" \
  --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
  --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac \
@@ -117,3 +125,19 @@ make
 make install
 make distclean
 hash -r
+sudo cp /var/www/nZEDbetter/misc/update_scripts/nix_scripts/tmux/powerline/powerline/themes/default.sh /var/www/nZEDbetter/misc/update_scripts/nix_scripts/tmux/powerline/powerline/themes/tmux.sh
+echo "export PATH=$HOME/bin:$PATH" >> ~/.bashrc
+echo 'alias tmux-dir="cd /var/www/nZEDbetter/misc/update_scripts/nix_scripts/tmux"' >> ~/.bash_aliases
+echo 'alias www-dir="cd /var/www/nZEDbetter/www"' >> ~/.bash_aliases
+echo 'alias misc-dir="cd /var/www/nZEDbetter/misc"' >> ~/.bash_aliases
+echo 'alias sqlstart="sudo service mysql start"' >> ~/.bash_aliases
+echo 'alias sqlstop="sudo service mysql stop"' >> ~/.bash_aliases
+echo 'alias sqllog="sudo tail -f /var/lib/mysql/mysql-error.log"' >> ~/.bash_aliases
+echo -e '\n\n\nCONGRATULATIONS - The setup script has completed.  Do not forget to do the following:\n'
+echo -e '	1. Set the default time zone in /etc/php5/cli/php.ini'
+echo -e '	2. Set the default time zone in /etc/php5/apache2/php.ini'
+echo -e '	3. Create a my.cnf and save it in /etc/mysql/my.cnf'
+echo -e '	   Go to http://tools.percona.com to create a base my.cnf tailored to your hardware\n'
+cd ~
+. .bashrc
+
