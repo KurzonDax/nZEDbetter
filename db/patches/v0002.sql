83a7de24846d3d5706739eb3c7bdbd36f82a5e69
Kurzon Dax <randy@thewilliscrew.org>
2013-11-29 16:49:31 -0600

    NZ-51 - v0002 SQL Patch Has Typo in Last Line

--- db/patches/v0002.sql
+++ db/patches/v0002.sql
@@ -2,4 +2,4 @@ INSERT INTO `nzedbetter`.`tmux` ( `ID` , `setting` , `value` , `updateddate` ) V
 INSERT INTO `nzedbetter`.`tmux` ( `ID` , `setting` , `value` , `updateddate` ) VALUES (NULL, 'NO_PURGE_MISC_BEFORE_FIX', 'FALSE', CURRENT_TIMESTAMP);
 INSERT INTO `nzedbetter`.`site` ( `ID` , `setting` , `value` , `updateddate` ) VALUES (NULL, 'NZEDBETTER_VERSION', '0.6', CURRENT_TIMESTAMP);
 INSERT INTO `nzedbetter`.`site` ( `ID` , `setting` , `value` , `updateddate` ) VALUES (NULL, 'postProcAddBooks', '0', CURRENT_TIMESTAMP);
-UPDATE `site` SET `VALUE = 'v0002' WHERE `setting` = 'sqlpatch';
+UPDATE `site` SET `VALUE` = 'v0002' WHERE `setting` = 'sqlpatch';
