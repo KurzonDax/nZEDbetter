dcb3d6c9faf2da71208747a4232e6e81dc8aa35d
Kurzon Dax <randy@thewilliscrew.org>
2013-12-08 23:11:18 -0600

    NZ-44 - Fix movieinfo for movies with no IMDB id

--- db/patches/v0003.sql
+++ db/patches/v0003.sql
@@ -1,4 +1,4 @@
 ALTER TABLE parts DROP `ID`, DROP INDEX parthash, ADD PRIMARY KEY (`parthash`);
-ALTER TABLE `movieinfo` CHANGE `imdbID` `imdbID` INT ( 7 ) NOT NULL;
+ALTER TABLE `movieinfo` CHANGE `imdbID` `imdbID` INT ( 8 ) NOT NULL;
 UPDATE `site` SET `VALUE` = '0.7' WHERE `setting` = 'NZEDBETTER_VERSION';
 UPDATE `site` SET `VALUE` = 'v0003' WHERE `setting` = 'sqlpatch';
