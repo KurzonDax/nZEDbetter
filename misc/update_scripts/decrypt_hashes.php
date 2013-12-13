e7186aaa6e6aaa4b1dab81f1d03558017176f608
Kurzon Dax <randy@thewilliscrew.org>
2013-12-13 14:01:12 -0600

    NZ-71 - Dehash not properly indicating searchname changes

--- misc/update_scripts/decrypt_hashes.php
+++ misc/update_scripts/decrypt_hashes.php
@@ -15,20 +15,29 @@ function preName()
 	$counter = 0;
 	$loops = 1;
 	$reset = 0;
+    $affectedRows = 0;
 	//$db->queryDirect("update releases set dehashstatus = -1 where dehashstatus = 0 and searchname REGEXP '[a-fA-F0-9]{32}'");
 
 	$db->query("update releases set dehashstatus = -1 where dehashstatus = 0 and searchname REGEXP '[a-fA-F0-9]{32}'");
-	if($res = $db->queryDirect("select ID, searchname from releases where dehashstatus between -6 and -1 and searchname REGEXP '[a-fA-F0-9]{32}'"))
+	if($res = $db->queryDirect("select ID, searchname from releases where dehashstatus between -6 and -1 and searchname REGEXP '[a-fA-F0-9]{32}|[a-fA-F0-9]{40}'"))
 	{
 		foreach ($res as $row)
 		{
 			$success = false;
-			if (preg_match('/([0-9a-fA-F]{32})/', $row['searchname'], $match))
+            preg_match("/([a-f0-9]{32}|[a-f0-9]{40})/i", $row['searchname'], $match);
+			if (isset($match[1]))
 			{
-				if($res1 = $db->queryOneRow(sprintf("select title from predb where md5 = %s", $db->escapeString($match[1]))))
+				$hash = $db->escapeString($match[1]);
+
+                if($res1 = $db->queryOneRow("select ID, title, source from predb where md5 =" . $hash . " OR md2=" . $hash . " OR md4=" . $hash . " OR ripemd128=" . $hash .
+                    " OR tiger128_3=" . $hash . " OR tiger128_4=" . $hash . " OR haval128_3=" . $hash . " OR haval128_4=" . $hash . " OR haval128_5=" . $hash .
+                    " OR ripemd160=" . $hash . " OR tiger160_3=" . $hash . " OR tiger160_4=" . $hash . " OR haval160_3=" . $hash . " OR haval160_4=" . $hash .
+                    " OR haval160_5=" . $hash))
 				{
 					$db->query(sprintf("update releases set dehashstatus = 1, relnamestatus = 6, searchname = %s where ID = %d", $db->escapeString($res1['title']), $row['ID']));
-					if ($db->getAffectedRows() >= 1)
+                    $affectedRows = $db->getAffectedRows();
+                    echo "Affected Rows: " . $affectedRows . "\n";
+                    if ($affectedRows >= 1)
 					{
 						echo "Renamed hashed release: ".$res1['title']."\n";
 						$success = true;
@@ -38,7 +47,7 @@ function preName()
 			}
 			if ($success == false)
 				$db->query(sprintf("update releases set dehashstatus = dehashstatus - 1 where ID = %d", $row['ID']));
-			$consoletools->overWrite("Renaming hashed releases:".$consoletools->percentString($loops++,mysqli_num_rows($res)));
+			$consoletools->overWrite("Attempting to dehash hashed releases:".$consoletools->percentString($loops++,mysqli_num_rows($res)));
 		}
 	}
 	echo "\n".$counter. " release(s) names changed.\n";
